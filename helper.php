fd5dfa465c3bf78d4d962a20b81231c7b8a4b359
Martin <mrtn.mtth@gmx.de>
2014-02-01 15:14:48 +0100

    Unified function for executing SQL queries with query as a parameter

--- helper.php
+++ helper.php
@@ -2,20 +2,35 @@
 defined('_JEXEC') or die('Restricted Access');
 
 class modReports2Oraculum {
-	
+
 	public function getNext() {
-		$avg = self::getOverallAvgTime() * 60;	// value in seconds
+		$avg = self::executeQuery(self::$qMeanTime) * 60;	// value in seconds
 		// can be NULL if MIN(date1) is 0000-00-00
 		if ($avg == 0) return false;
-		$last = self::getLatestTimestamp();	// timestamp format: YYYY-MM-DD HH:MM:SS
+		$last = self::executeQuery(self::$qLatestTimestamp);	// timestamp format: YYYY-MM-DD HH:MM:SS
 		$next = strtotime($last.' + '.$avg.' seconds');	// unix timestamp in integer
 		while ($next < time()) {
 			$next = $next + $avg;
 		}
 		return $next;
 	}
-	
-	public function getNextType($next) {
+
+	// All the SQL queries
+	private static $qMeanTime = '	SELECT
+  										CASE
+											WHEN COUNT(date1) < 2 THEN 0
+											ELSE ROUND(
+												TIMESTAMPDIFF(
+											    MINUTE,
+    											MIN(date1), 
+												MAX(date1)) / (COUNT(date1)-1))
+										END
+										as mean_time
+									FROM #__reports		';
+
+	private static $qLatestTimestamp = 'SELECT MAX(date1) FROM #__reports';
+
+/*	public function getNextType($next) {
 		$query = 'SELECT MAX(id) FROM #__reports_data';
 		$j = self::query($query);
 		$results = array();
@@ -35,40 +50,8 @@ class modReports2Oraculum {
 		}
 		return $results;
 	}
-	
-	private function getOverallAvgTime() {
-		$db = JFactory::getDBO();
-		
-		// returns average time between each report in minutes
-		$query = 'SELECT
-  				CASE
-					WHEN COUNT(date1) < 2 THEN 0
-					ELSE ROUND(
-						TIMESTAMPDIFF(
-					    MINUTE,
-    					MIN(date1), 
-						MAX(date1)) / (COUNT(date1)-1))
-				END
-				as mean_time
-			FROM #__reports';
-		$db->setQuery($query);
-		$result = $db->loadResult();
-		
-		return $result;
-	}
-	
-	private function getLatestTimestamp() {
-		$db = JFactory::getDBO();
-		
-		$query = 'SELECT MAX(date1) FROM #__reports';
-		
-		$db->setQuery($query);
-		$result = $db->loadResult();
-		
-		return $result;
-	}
-	
-	private function query($query) {
+*/
+	private function executeQuery($query) {
 		$db = JFactory::getDBO();
 		$db->setQuery($query);
 		$result = $db->loadResult();		
--- tmpl/default.php
+++ tmpl/default.php
@@ -2,4 +2,4 @@
 defined('_JEXEC') or die('Restricted Access'); ?>
 <?php echo 'Der nächste Einsatz für die Feuerwehr Rethen ist am '.date('d.m.Y', $next).' um '.date('H:i', $next).' Uhr.'; ?>
 <br /><br /><span style="font-style:italic;">Das Orakel übernimmt für diese Angaben keine Gewähr.</span>
-<!--DEBUG:--?php print_r($array); ?-->
+<?php /*DEBUG: print_r($array);*/?>
