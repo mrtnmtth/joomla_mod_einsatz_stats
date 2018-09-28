1f7ab8570b54c6c415bdf24268a6348e8cb62406
mrtnmtth <mrtn.mtth@gmx.de>
2018-09-28 16:55:28 +0200

    move each query to its own respective function

--- helper.php
+++ helper.php
@@ -11,10 +11,10 @@ defined('_JEXEC') or die('Restricted Access');
 class ModEinsatzStatsHelper {
 
 	public static function getNext() {
-		$avg = self::executeQuery(self::$qMeanTime) * 60;	// value in seconds
+		$avg = self::queryReportsTimeMean() * 60;	// value in seconds
 		// can be NULL if MIN(date1) is 0000-00-00
 		if ($avg == 0) return false;
-		$last = self::executeQuery(self::$qLatestTimestamp);	// timestamp format: YYYY-MM-DD HH:MM:SS
+		$last = self::queryReportsTimeLatest();	// timestamp format: YYYY-MM-DD HH:MM:SS
 		$next = strtotime($last.' + '.$avg.' seconds');	// unix timestamp in integer
 		while ($next < time()) {
 			$next = $next + $avg;
@@ -23,22 +23,11 @@ class ModEinsatzStatsHelper {
 	}
 
 	public static function getStatsByType($year) {
-		$latest = substr(self::executeQuery(self::$qLatestTimestamp), 0, 4);
+		$latest = substr(self::queryReportsTimeLatest(), 0, 4);
 		if (($year > $latest) or !(ctype_digit($year)))
 			$year = $latest;
+		$result = self::queryReportsXType($year);
 
-		$db = JFactory::getDbo();
-		$query =
-			'SELECT arten.title AS label,
-				count(data1) AS value,
-				arten.marker AS color
-			FROM #__eiko_einsatzberichte AS berichte
-			INNER JOIN #__eiko_einsatzarten AS arten
-			ON berichte.data1=arten.id
-			WHERE berichte.state=1 AND berichte.date1 LIKE '.$db->quote($year.'%').'
-			GROUP BY data1
-			ORDER BY arten.ordering;';
-		$result = self::executeQuery($query, 1);
 		$data = new StdClass;
 		$data->datasets[] = new StdClass;
 		foreach ($result as $i) {
@@ -52,28 +41,13 @@ class ModEinsatzStatsHelper {
 	}
 
 	public static function getYears() {
-		$db = JFactory::getDbo();
-		$query =
-			'SELECT id, title, marker AS color
-			FROM #__eiko_einsatzarten
-			WHERE state=1
-			ORDER BY ordering;';
-		$categories = $db->setQuery($query)->loadObjectList();
-
-		$query =
-			'SELECT data1 AS id,
-				YEAR(date1) AS year,
-				count(data1) AS value
-			FROM #__eiko_einsatzberichte
-			WHERE state=1
-			GROUP BY data1, YEAR(date1)
-			ORDER BY year, id;';
-		$results = $db->setQuery($query)->loadObjectList();
+		$types = self::queryTypes();
+		$reports = self::queryReportsByTypeAndYear();
 
 		$years = array();
 		/* Restructure result data by years first to be later able to see if there
 		   is no data for a certain year in a certain category */
-		foreach ($results as $r) {
+		foreach ($reports as $r) {
 			if (!array_key_exists($r->year, $years))
 				$years[$r->year] = array();
 			$years[$r->year][$r->id] = $r->value;
@@ -81,17 +55,17 @@ class ModEinsatzStatsHelper {
 
 		$data = new StdClass;
 		$data->labels = array_keys($years);
-		foreach ($categories as $i => $cat) {
+		foreach ($types as $i => $t) {
 			$data->datasets[$i] = new StdClass;
-			$data->datasets[$i]->label = $cat->title;
-			$data->datasets[$i]->backgroundColor = $cat->color . 'bb';
+			$data->datasets[$i]->label = $t->title;
+			$data->datasets[$i]->backgroundColor = $t->color . 'bb';
 			$data->datasets[$i]->borderWidth = 1;
-			$data->datasets[$i]->borderColor = $cat->color;
-			$data->datasets[$i]->hoverBackgroundColor = $cat->color;
+			$data->datasets[$i]->borderColor = $t->color;
+			$data->datasets[$i]->hoverBackgroundColor = $t->color;
 			$data->datasets[$i]->data = array();
 			foreach ($data->labels as $y) {
-				if (array_key_exists($cat->id, $years[$y]))
-					$data->datasets[$i]->data[] = (int) $years[$y][$cat->id];
+				if (array_key_exists($t->id, $years[$y]))
+					$data->datasets[$i]->data[] = (int) $years[$y][$t->id];
 				else
 					$data->datasets[$i]->data[] = 0;
 			}
@@ -119,28 +93,70 @@ class ModEinsatzStatsHelper {
 		return http_response_code(400);
 	}
 
-	// All the SQL queries
-	private static $qMeanTime = '	SELECT
-  										CASE
-											WHEN COUNT(date1) < 2 THEN 0
-											ELSE ROUND(
-												TIMESTAMPDIFF(
-											    MINUTE,
-    											MIN(date1),
-												MAX(date1)) / (COUNT(date1)-1))
-										END
-										as mean_time
-									FROM #__eiko_einsatzberichte	';
 
-	private static $qLatestTimestamp = 'SELECT MAX(date1) FROM #__eiko_einsatzberichte';
+	private static function queryReportsTimeLatest() {
+		$db = JFactory::getDbo();
+		$query = 'SELECT MAX(date1) FROM #__eiko_einsatzberichte';
+		$result = $db->setQuery($query)->loadResult();
+		return $result;
+	}
 
-	private static function executeQuery($query, $returnArray=0) {
-		$db = JFactory::getDBO();
-		$db->setQuery($query);
-		if ($returnArray)
-			$result = $db->loadObjectList();
-		else
-			$result = $db->loadResult();
+	private static function queryReportsTimeMean() {
+		$db = JFactory::getDbo();
+		$query =
+			'SELECT
+				CASE
+				WHEN COUNT(date1) < 2 THEN 0
+				ELSE ROUND(
+					TIMESTAMPDIFF(
+						MINUTE,
+						MIN(date1),
+					MAX(date1)) / (COUNT(date1)-1))
+				END
+			AS mean_time
+			FROM #__eiko_einsatzberichte	';
+		$result = $db->setQuery($query)->loadResult();
+		return $result;
+	}
+
+	private static function queryReportsByTypeAndYear() {
+		$db = JFactory::getDbo();
+		$query =
+			'SELECT data1 AS id,
+				YEAR(date1) AS year,
+				count(data1) AS value
+			FROM #__eiko_einsatzberichte
+			WHERE state=1
+			GROUP BY data1, YEAR(date1)
+			ORDER BY year, id;';
+		$result = $db->setQuery($query)->loadObjectList();
+		return $result;
+	}
+
+	private static function queryReportsXType($year) {
+		$db = JFactory::getDbo();
+		$query =
+			'SELECT arten.title AS label,
+				count(data1) AS value,
+				arten.marker AS color
+			FROM #__eiko_einsatzberichte AS berichte
+			INNER JOIN #__eiko_einsatzarten AS arten
+			ON berichte.data1=arten.id
+			WHERE berichte.state=1 AND berichte.date1 LIKE '.$db->quote($year.'%').'
+			GROUP BY data1
+			ORDER BY arten.ordering;';
+		$result = $db->setQuery($query)->loadObjectList();
+		return $result;
+	}
+
+	private static function queryTypes() {
+		$db = JFactory::getDbo();
+		$query =
+			'SELECT id, title, marker AS color
+			FROM #__eiko_einsatzarten
+			WHERE state=1
+			ORDER BY ordering;';
+		$result = $db->setQuery($query)->loadObjectList();
 		return $result;
 	}
 }
