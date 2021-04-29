e0f689be7addd0ab3498d97f129a05f1092c5cdd
mrtnmtth <mrtn.mtth@gmx.de>
2021-04-29 20:55:58 +0200

    chore: upgrade to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -8,5 +9,10 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/mod_einsatz_stats.js'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({
+            extractComments: false,
+        })],
     }
 };
