ce161a40806c58b534b54a6abf6d75b33d5e02da
mrtnmtth <mrtn.mtth@gmx.de>
2021-08-02 13:42:31 +0200

    feat: unload Mootools JS libs to prevent conflicts

--- src/mod_einsatz_stats.php
+++ src/mod_einsatz_stats.php
@@ -10,10 +10,13 @@ defined('_JEXEC') or die('Restricted Access');
 
 JLoader::register('ModEinsatzStatsHelper', __DIR__ . '/helper.php');
 
+/**
+ * @var \Joomla\CMS\Object\CMSObject $params
+ */
 $mode = $params->get('mode', '1');
 $pie_size = $params->get('pie_size', '175');
 $pie_legend = $params->get('pie_legend', '1') ? 'true' : 'false';
-$all_stats = $params->get('all_stats', '1') == 1 ? true : false;
+$all_stats = $params->get('all_stats', '1') == 1;
 
 switch ($mode) {
     case 0:
@@ -23,6 +26,14 @@ switch ($mode) {
     case 1:
         JHtml::_('jquery.framework', false);
         $doc = JFactory::getDocument();
+
+        // remove Mootools libs because of conflicts with modern JS
+        foreach ($doc->_scripts as $path => $attribs) {
+            if (strstr($path, 'mootools') !== false) {
+                unset($doc->_scripts[$path]);
+            }
+        }
+
         $doc->addScriptDeclaration('const showLegend = ' . $pie_legend . ';');
         $doc->addScript(JURI::root() . 'modules/mod_einsatz_stats/js/mod_einsatz_stats.js');
         break;
