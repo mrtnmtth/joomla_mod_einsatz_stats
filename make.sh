9671a173b2498c9a7634652cec36a8c64b77cd86
mrtnmtth <mrtn.mtth@gmx.de>
2021-10-05 20:59:31 +0200

    build: add function to tag releases

--- make.sh
+++ make.sh
@@ -35,16 +35,35 @@ package () {
 }
 
 tag-release () {
-  if [[ -z "$@" ]]; then
+  if [[ -z "$1" ]]; then
     echo "no version number given"
     exit 0;
   fi
+  if ! [[ $1 == v* ]]; then
+    echo "version tag needs to start with a 'v'"
+    exit 0
+  fi
 
-	# TODO
+  version=$1
+	date=$(LC_ALL=en_GB.utf8 date +'%B %Y')
+
+  # replace version and date in extension.xml
+  sed -i -e "s/<version>.*<\/version>/<version>${version#v}<\/version>/g" \
+    -e "s/<creationDate>.*<\/creationDate>/<creationDate>${date}<\/creationDate>/g" \
+    src/mod_einsatz_stats.xml
+
+  # replace version and release url in update.xml
+  sed -i -e "s/<version>.*<\/version>/<version>${version#v}<\/version>/g" \
+    -e "s/download\/.*\/mod-einsatz-stats-.*\.zip/download\/${version}\/mod-einsatz-stats-${version#v}\.zip/g" \
+    update.xml
+
+  git add src/mod_einsatz_stats.xml update.xml
+  git commit -m "release: prepare ${version}"
+  git tag "${version}"
 }
 
 case $CMD in
   build)      build;;
   package)    package $@;;
-  relase)     tag-release $@;;
+  release)    tag-release $@;;
 esac
